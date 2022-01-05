c1580375b7a7f0a88c8c5339782eec48d369faa2
cjpen <cjpen@me.com>
2022-01-04 16:45:04 -0800

    update started developing render

--- js/main.js
+++ js/main.js
@@ -12,24 +12,44 @@ let round = 1;
 let isPlaying = false;
 
 /*----- cached element references -----*/
-let scoreboard = 
-let 
+const buttonEls = document.querySelectorAll('#board > div');
+const startButton = document.getElementById('start'); // start button
+const scoreboard = document.getElementById('score'); // current score (equals the current round)
 
 /*----- event listeners -----*/
-
+startButton.addEventListener('click', (event) => {
+    gameTurn()
+}); // start game
+// player input click
 
 /*----- functions -----*/
 
+console.log(buttonEls);
+
 function gameTurn() {
+    console.log("clicked");
     let playerPattern = [];
 
-    gamePattern.push(Math.floor(Math.random * 4));
+    gamePattern.push(Math.floor(Math.random() * 4));
+    
+    renderGamePattern();
+    // console.log(gamePattern);
 };
 
 function changeTurn() {
     turn = turn * -1;
-}
+};
 
 function render() {
 
+};
+
+function renderGamePattern() {
+    for (let i = 0; i < gamePattern.length; i++) {
+        let litIdx = gamePattern[i]
+        setTimeout(function() {
+            buttonEls[litIdx].style.opacity = .5;
+        }, 1000);
+        
+    }
 };
