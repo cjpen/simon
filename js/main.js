010060766d9040c9bfbb50fd84bd43da9bf0a4ca
cjpen <cjpen@me.com>
2022-01-04 18:06:20 -0800

    add functionallity for gamePattern and playerPatte

--- js/main.js
+++ js/main.js
@@ -1,7 +1,29 @@
 /*----- constants -----*/
-const colors = [
-    'green', 'red', 'yellow', 'blue'
+const colors = [ 
+    {
+        color: 'green', 
+        default: '#009900',
+        lit: '#1fff1f'
+    },
+    {
+        color: 'red',
+        default: '#990000',
+        lit: '#ff1f1f'
+    },
+    {
+        color: 'yellow',
+        default: '#999900',
+        lit: '#ffff1f'
+    },
+    {
+        color: 'red',
+        default: '#000099',
+        lit: '#1f1fff'
+    }
 ];
+
+const litTime = 750
+const gapTime = 250
 
 
 /*----- app's state (variables) -----*/
@@ -12,7 +34,7 @@ let round = 1;
 let isPlaying = false;
 
 /*----- cached element references -----*/
-const buttonEls = document.querySelectorAll('#board > div');
+const buttonEls = Array.from(document.querySelectorAll('#board > div'));
 const startButton = document.getElementById('start'); // start button
 const scoreboard = document.getElementById('score'); // current score (equals the current round)
 
@@ -20,7 +42,13 @@ const scoreboard = document.getElementById('score'); // current score (equals th
 startButton.addEventListener('click', (event) => {
     gameTurn()
 }); // start game
-// player input click
+document.getElementById("board").addEventListener('click', event => {
+    const button = event.target;
+    const buttonIdx = buttonEls.indexOf(button);
+    playerPattern.push(buttonIdx);
+    console.log(playerPattern);
+} // player input click
+);
 
 /*----- functions -----*/
 
@@ -33,11 +61,19 @@ function gameTurn() {
     gamePattern.push(Math.floor(Math.random() * 4));
     
     renderGamePattern();
-    // console.log(gamePattern);
+    changeTurn ();
+};
+
+function playerTurn () {
+    console.log("player turn");
+    changeTurn();
 };
 
 function changeTurn() {
     turn = turn * -1;
+    if ( turn === -1) {
+        playerTurn ();
+    } else gameTurn ();
 };
 
 function render() {
@@ -45,11 +81,38 @@ function render() {
 };
 
 function renderGamePattern() {
-    for (let i = 0; i < gamePattern.length; i++) {
-        let litIdx = gamePattern[i]
-        setTimeout(function() {
-            buttonEls[litIdx].style.opacity = .5;
-        }, 1000);
-        
+    isPlaying = true;
+    let idx = 0;
+    const timerId = setInterval(function() {
+        for (let i = 0; i < gamePattern.length; i++) {
+            let litIdx = gamePattern[i];
+            buttonEls[litIdx].style.backgroundColor = colors[litIdx].lit;
+        }
+    setTimeout(function() {
+        for (let i = 0; i < gamePattern.length; i++) {
+            let litIdx = gamePattern[i];
+            buttonEls[litIdx].style.backgroundColor = colors[litIdx].default;
+        }
+    }, litTime);
+    idx++;
+    if (idx === gamePattern.length) {
+      clearInterval(timerId);
+      isPlaying = false;
+      setTimeout(litTime);
     }
+  }, litTime + gapTime);
+
+
+    // const timerID = setInterval(function(){
+    //     let litIdx = gamePattern[i];
+
+    // })
+    // for (let i = 0; i < gamePattern.length; i++) {
+    //     let litIdx = gamePattern[i]
+    //     setTimeout(function() {
+    //         buttonEls[litIdx].style.backgroundColor = colors[litIdx].lit;
+    //         buttonEls[litIdx].style.backgroundColor = colors[litIdx].default;
+    //     }, 500);
+// 
+    // }
 };
